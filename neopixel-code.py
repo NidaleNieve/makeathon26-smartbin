fea3c3d19e2fab5c6ba6896e15d2cd0a8e99d094
NidaleNieve <nidalenieve@gmail.com>
2026-02-27 12:29:11 +0000

    neopixel upload

---
+++ neopixel-code.py
@@ -0,0 +1,12 @@
+from machine import Pin
+from neopixel import NeoPixel
+from time import sleep_ms
+
+neo = NeoPixel(Pin(4), 41)   #  8 Leds (0 - 7)
+
+# slökktu á öllum leds
+neo.fill([0, 0, 0])
+
+# Allar NeoPixel perurnar lýsa rauðu ljósi í eina sekúndu með fill aðferð.
+neo.fill([255, 255, 255])
+neo.write()
