61d78d5f2ebac21a76ded5dec2681b09c95c674c
Roseplate <66807484+Roseplate@users.noreply.github.com>
2026-02-27 10:47:38 +0000

    cleanup

--- esp_connection.py
+++ esp_connection.py
@@ -1,6 +1,5 @@
 # Main Module meant for communication with our ESP
 
-#class Controller
 import time
 import asyncio
 from bleak import BleakClient, BleakScanner
@@ -10,7 +9,7 @@ UART_WRITE_UUID = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E" # Pi Writes to this
 UART_NOTIFY_UUID = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E" # Pi Reads/Notifies from this
 
 class Connection:
-    items_to_send = []
+    items_to_send = ["active"]
     attachments = []
 
     def __init__(self):
@@ -22,7 +21,6 @@ class Connection:
     def attach(self, value: str, function):
         Connection.attachments.append((value, function))
 
-
 # 1. THE LISTENER FUNCTION
 # This triggers automatically whenever the ESP32 sends a packet back
 async def notification_handler(sender, data):
@@ -32,7 +30,6 @@ async def notification_handler(sender, data):
             function()
 
 async def running():
-    
     print("Scanning for ESP32_BLE...")
     # Find the ESP32 by the name we gave it
 
@@ -51,37 +48,23 @@ async def running():
 
     
     async with BleakClient(device) as client:
+        # Start listening for incoming packets from the ESP32
         await client.start_notify(UART_NOTIFY_UUID, notification_handler)
 
-
-
-
-    
-        
-        # Start listening for incoming packets from the ESP32
-        
-
         # 2. THE SENDER FUNCTION
-        # A list of basic strings to send
-        commands_to_send = ["left", "right", "up", "down", "none"]
-        
-        for item in Connection.items_to_send:
-            # Write the string to the ESP32
-            
-            await client.write_gatt_char(UART_WRITE_UUID, item.encode('utf-8'))
-            # Wait 3 seconds before sending the next one 
-            # (Allows time for the ESP to receive, process, and reply)
-            await asyncio.sleep(3)
+        while True:
+            # Get the item from the list
+            if len(Connection.items_to_send) > 0:
+                item = Connection.items_to_send.pop(0)
+                
+                # Write the string to the ESP32
+                try:
+                    await client.write_gatt_char(UART_WRITE_UUID, item.encode('utf-8'))
+                except:
+                    # if it failed to send we place it back in the queue where we took it.
+                    Connection.items_to_send.insert(0, item)
 
-            ### print("Finished sending all commands. Listening for 5 more seconds...")
-            ### await asyncio.sleep(5)
-            
-            # Stop listening before disconnecting
-            ### await client.stop_notify(UART_NOTIFY_UUID)
 
 async def start():
-    # Run the async loop
     asyncio.create_task(running())
-    #asyncio.create_task(running)
 
-# 34:85:18:A6:43:BA ESP MAC
