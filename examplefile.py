6ab22158008970e92d87f365fe23a760f2112c46
Roseplate <66807484+Roseplate@users.noreply.github.com>
2026-02-27 15:09:12 +0000

    motion input

--- examplefile.py
+++ examplefile.py
@@ -4,8 +4,12 @@ import asyncio
 import esp_connection
 
 # Define certain callback functions
-def callback_func():
-    print("test received!")
+def test():
+    print("test!")
+
+def motion():
+    # Motion code
+    print("would activate machine vision here")
 
 # Unfortunately, I was not able to make async disappear, please run yer code like this when using the connection module.
 async def main():
@@ -14,13 +18,16 @@ async def main():
     connection = esp_connection.Connection()
 
     # Attach certain callback functions to certain messages, the connection will run the callback when that message is received
-    connection.attach("test", callback_func)
+    connection.attach("test", test)
+    connection.attach("motion", motion)
+
     print("running")
 
     await asyncio.sleep(1)
 
     # We can send our own messages, here we send down, which prompts the esp test code to send back "test"
-    connection.send("down")
+    connection.send("test")
+
 
     # Connection will remain open while main is still running
     while True:
