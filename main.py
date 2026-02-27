9d5851862ac8112d3bef5f0a62d3c29a60499930
Roseplate <66807484+Roseplate@users.noreply.github.com>
2026-02-27 15:10:07 +0000

    send motion on motion

--- main.py
+++ main.py
@@ -6,28 +6,24 @@ from machine import Pin
 
 print("Start program.")
 
+motion_sensor = Pin(5, Pin.IN)
 servo = Servo(Pin(6), freq=50, min_us=500, max_us=2500)
 
 connection = rasp_connection.Connector()
 
 # Example Usage, feel free to change or comment out whenever you need to use main for something
-def left():
-    print("left!")
 
-def right():
-    print("right!")
-
-def up():
-    print("up!")
-
-def down():
-    print("down!")
+def test():
+    print("test!")
     connection.send("test")
 
-connection.attach("left", left)
-connection.attach("right", right)
-connection.attach("up", up)
-connection.attach("down", down)
+def send_motion(p):
+    print("motion found")
+    connection.send("motion")
+
+# Command Attachments/Callbacks & IRQ Callbacks
+connection.attach("test", test)
+motion_sensor.irq(send_motion, Pin.IRQ_RISING)
 
 #Start position
 servo.write_angle(0)
